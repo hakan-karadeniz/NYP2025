2f9f853edf7bd9d5c04e9d8acc31cc2e4a743dfa
hakan-karadeniz <karadenizhakan007@gmail.com>
2025-05-28 19:48:27 +0300

    NYP2025
    Öğrenci Kayıt ve Sınıf Takip Sistemi

---
+++ NesneProje/Info.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NesneProje
+{
+    internal class Info
+    {
+        public int Student_No { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
---
+++ NesneProje/Lessons.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NesneProje
+{
+    internal class Lessons : Info
+    {
+        public double Matematik { get; set; }
+        public double Fizik { get; set; }
+        public double Kimya { get; set; }
+        public double Biyoloji { get; set; }
+    }
+}
